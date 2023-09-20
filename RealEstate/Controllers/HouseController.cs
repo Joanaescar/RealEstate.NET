655be9bd4020b57d75f1ee4ff3630908f7c12a5d
Joanaescar <joanaa-esteves@hotmail.com>
2023-09-20 11:17:30 +0100

    Created reserve and unreserve endpoints

--- RealEstate/Controllers/HouseController.cs
+++ RealEstate/Controllers/HouseController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RealEstate.DTO;
 using RealEstate.Entites;
+using RealEstate.Exceptions;
 using RealEstate.Respositories;
+using RealEstate.Services;
 
 namespace RealEstate.Controllers
 {
@@ -9,25 +11,61 @@ namespace RealEstate.Controllers
     [Route("[controller]")]
     public class HouseController : ControllerBase // como extende do controller base ela adiciona automaticamente a dependencia
     {
-        private readonly IHouseRespository _houseRepository;
-        public HouseController(IHouseRespository houseRespository)
+        private readonly IHouseService _houseService;
+        public HouseController(IHouseService houseService)
         {
-            _houseRepository = houseRespository;
+            _houseService = houseService;
         }
 
         [HttpGet(Name = "GetHouses")] // é uma identificação
         public async Task<IEnumerable<House>> GetAllAsync() // é um get que me vai retorna todas as houses que tiverem no repositorio
         {
-            return await _houseRepository.FindAllAsync();
+            return await _houseService.ListAsync();
         }
 
         [HttpPost(Name = "AddHouse")]
         public async Task<House> AddAsync([FromBody] HouseDTO houseDTO)
         {
             House house = HouseDTO.fromDTO(houseDTO);
-            House entity = await _houseRepository.AddAsync(house);
-            await _houseRepository.SaveAsync();
+            House entity = await _houseService.RegisterAsync(house);
             return entity;
+        }
+
+        [HttpDelete(Name = "DeleteHouse")]
+        public async Task DeleteAsync([FromRoute] int id)
+        {
+            await _houseService.DeleteAsync(id);
+        }
+
+        [HttpPut("Reserve/{id}", Name = "ReserveHouse")]
+        public async Task ReserveAsync([FromRoute] int id, [FromBody] UserDto userDto)
+        {
+            User user = UserDto.fromDto(userDto);
+
+            if(user != null)
+            {
+                await _houseService.ReserveAsync(id, user.Id);
+            }
+            else
+            {
+                throw new UserNotFoundException("User not found.");
+            }
+       
+        }
+
+        [HttpPut("Unreserve/{id}", Name = "UnreserveHouse")]
+        public async Task UnreserveAsync([FromRoute] int id, [FromBody] UserDto userDto)
+        {
+            User user = UserDto.fromDto(userDto);
+
+            if (user != null)
+            {
+                await _houseService.UnreserveAsync(id, user.Id);
+            }
+            else
+            {
+                throw new UserNotFoundException("User not found.");
+            }
         }
     }
 }
